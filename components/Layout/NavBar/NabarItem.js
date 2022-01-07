5ddc281d7dd0b9bfcd99032a4cc2ca077aa4a450
reza <amir.torangan@gmail.com>
2022-01-08 02:19:05 +0330

    navbarItem ring color changed

--- components/Layout/NavBar/NabarItem.js
+++ components/Layout/NavBar/NabarItem.js
@@ -8,7 +8,7 @@ const NavbarItem = ({ href, title }) => {
       <Link href={href}>
         <a className="m-1">
           <Button
-            className={`w-full py-0.5 px-1.5 m-0 active:ring-2 ring-offset-2 ring-cyan-500 transition-all ease-linear duration-150 hover:animate-pulse ${
+            className={`w-full py-0.5 px-1.5 m-0 active:ring-2 ring-offset-2 ring-indigo-700 transition-all ease-linear duration-150 hover:animate-pulse ${
               router.pathname == href &&
               "bg-gradient-to-tr from-black to-indigo-700 text-white hover:animate-none"
             }`}
