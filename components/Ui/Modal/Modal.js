db805ebb7d428f04791a78a20c5e6f370e2a67cf
reza <amir.torangan@gmail.com>
2022-01-08 02:20:25 +0330

    modal component background bug fixed and animated

--- components/Ui/Modal/Modal.js
+++ components/Ui/Modal/Modal.js
@@ -1,14 +1,19 @@
 import Card from "../Card/Card";
+import { motion } from 'framer-motion';
 
 const Modal = ({ children, className, onExit,isShown }) => {
-
+  let backdrop;
+  !isShown ? backdrop={opacity:1} : backdrop={opacity:0}
   return (
     <>
-      <Card className={`z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 m-0  max-w-lg transition-all ease-linear delay-500 duration-300 ${className}`}>
+      <Card initial={{opacity:0,scale:0,y:"-50%",borderRadius:"100%"}} animate={{opacity:1,scale:1,borderRadius:"1rem"}} 
+      className={`z-50 fixed top-1/2 left-0  right-0 transform -translate-y-1/2  mx-8  max-w-lg transition-all ease-linear delay-500 duration-300 bg-white ${className}`}>
        {children}
       </Card>
-      <div
-        className="fixed top-0 bottom-0 left-0 right-0 bg-black/40 backdrop-blur z-40 cursor-pointer transition-all ease-linear delay-100 duration-150"
+      <motion.div 
+      initial={{opacity:0}}
+      animate={backdrop}
+        className="fixed top-0 bottom-0 left-0 right-0 bg-black/40 backdrop-blur z-40 cursor-pointer"
         onClick={onExit}
       />
     </>
