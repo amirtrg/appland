d2f58b554d91b1d28f7d0931aed8925362dd0429
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-25 13:23:05 +0430

    [IMPROVMENT] chnage nav item on router onReady

--- components/Layout/NavBar/NabarItem.js
+++ components/Layout/NavBar/NabarItem.js
@@ -3,20 +3,20 @@ import { useRouter } from "next/router";
 import Button from "./../../Ui/Button/Button";
 
 const NavbarItem = ({ href, title }) => {
-    const router = useRouter();
-    return (
-      <Link href={href}>
-        <a className="m-1">
-          <Button
-            className={`w-full py-0.5 px-1.5 m-0 active:ring-2 ring-offset-2 ring-indigo-700 transition-all ease-linear duration-150 hover:animate-pulse dark:text-white ring-offset-[#121212] ${
-              router.pathname == href &&
-              "bg-gradient-to-tr from-black to-indigo-700 text-white hover:animate-none"
-            }`}
-          >
-            {title}
-          </Button>
-        </a>
-      </Link>
-    );
-  };
-  export default NavbarItem
+  const router = useRouter();
+  return (
+    <Link href={href}>
+      <a className="m-0">
+        <Button
+          className={`w-full py-0 px-1.5 m-0 active:ring-2 ring-offset-2 ring-indigo-700 transition-all ease-linear duration-150 hover:animate-pulse text-blue-500/50 dark:text-white/50 ring-offset-[#121212] ${
+            router.isReady&& router.pathname == href &&
+            "text-blue-500/100 dark:text-white/100 hover:animate-none"
+          }`}
+        >
+          {title}
+        </Button>
+      </a>
+    </Link>
+  );
+};
+export default NavbarItem;
