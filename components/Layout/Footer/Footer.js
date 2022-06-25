4e51c399f14358f3791e090099c5763eb67e8611
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-25 13:21:21 +0430

    [IMPROVMENT] change footer ui

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -1,43 +1,78 @@
 import Card from "../../Ui/Card/Card";
 import Button from "../../Ui/Button/Button";
 import { memo } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 left-0 right-0 bg-gradient-to-tr from-indigo-900 to-indigo-800 my-0 mx-0 shadow-none p-4 text-white dark:to-transparent dark:from-slate-900 dark:bg-gradient-to-t">
-      <div className=" m-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  lg:w-3/4 text-center">
+    <footer className=" bg-slate-900 my-0 mx-0 shadow-none p-4 text-white dark:to-transparent dark:from-slate-900 dark:bg-gradient-to-t">
+      <div className=" m-auto grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4  lg:w-3/4 text-center">
         <div className="text-right">
-          <h3 className="text-2xl">
-         اپلند
-          </h3>
+          <h3 className="text-2xl font-bold">اپلند</h3>
           <p className="text-sm text-white text-opacity-70">
-           ساده ترین تجربه سفارش صنایع دستی
+            ساده ترین تجربه سفارش کالای دیجیتال
           </p>
         </div>
-        <div className="grid grid-cols-2 ">
+        <div className="grid grid-cols-2 gap-2">
           <h3 className="text-lg col-span-full">تماس با ما</h3>
-      
-          <Button type="link" className="text-xs m-0 px-0 py-0 hover:text-black ">مارا دنبال کنید</Button>
-          <p>شماره های تماس</p>
+
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            مارا دنبال کنید
+          </Button>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            شماره های تماس
+          </Button>
         </div>
-        <div className="grid grid-cols-2">
-          <h3 className="text-xl text-center col-span-2">درباره ما</h3>
-          <Link href="/about-us">درباره ما</Link>
-          <p>
-          تیم اپلند
-          </p>
+        <div className="grid grid-cols-2 gap-2">
+          <h3 className="text-lg text-center col-span-2">درباره ما</h3>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            درباره ما
+          </Button>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            تیم اپلند
+          </Button>
         </div>
-        <div className="grid grid-cols-2 ">
-          <h3 className="text-xl text-center col-span-2">خدمات</h3>
-          <p>شرایط و مقررات</p>
-          <p>شرایط گارانتی</p>
-          <p>شرایط عودت</p>
-          <p>شرایط تعویض</p>
+        <div className="grid grid-cols-2 gap-2 ">
+          <h3 className="text-lg text-center col-span-2">خدمات</h3>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            شرایط و مقررات
+          </Button>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            شرایط گارانتی
+          </Button>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            شرایط عودت
+          </Button>
+          <Button
+            type="link"
+            className="text-sm m-0 px-0 py-0 hover:text-black "
+          >
+            شرایط تعویض
+          </Button>
         </div>
-        <p className="text-sm col-span-full mt-4">
-          تمام حقوق مادی و معنوی و محتوایی این وبسایت برای تیم اپلند محفوظ
-          است
+        <p className="text-xs col-span-full gap-2 mt-4">
+          تمام حقوق مادی و معنوی و محتوایی این وبسایت برای تیم اپلند محفوظ است
         </p>
       </div>
     </footer>
