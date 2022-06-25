5e18c982a4e8d0e65ebd860215fe1cdf4ce7df36
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-25 13:21:33 +0430

    [IMPROVMENT] menu modal ui

--- components/Layout/NavBar/MenuModal.js
+++ components/Layout/NavBar/MenuModal.js
@@ -2,11 +2,11 @@ import Button from "../../Ui/Button/Button";
 import Modal from "./../../Ui/Modal/Modal";
 import NavbarItem from './NabarItem';
 
-export default function MenuModal({onExit}) {
+export default function MenuModal({onExit,isShown}) {
   return (
-    <Modal className="flex flex-col" onExit={onExit}>
-      <h1 className="text-2xl border-b border-black/50 dark:border-white pb-2">
-        گالری نصف جهان
+    <Modal className="flex flex-col" onExit={onExit} isShown={isShown}>
+      <h1 className="text-2xl border-b text-white font-bold border-white/50 dark:border-white pb-2">
+        اپلند
       </h1>
       <NavbarItem href="/" title="صفحه اصلی" />
       <NavbarItem href="/products" title="محصولات" />
