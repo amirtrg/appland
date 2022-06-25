a061cf9c62c2f0038fc866c463287f953b892b36
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-25 13:23:56 +0430

    [IMPROVMENT] [UI] change modal component

--- components/Ui/Modal/Modal.js
+++ components/Ui/Modal/Modal.js
@@ -1,20 +1,24 @@
 import Card from "../Card/Card";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-const Modal = ({ children, className, onExit,isShown }) => {
+const Modal = ({ children, className, onExit, isShown }) => {
   let backdrop;
-  !isShown ? backdrop={opacity:1} : backdrop={opacity:0}
+  !isShown ? (backdrop = { opacity: 0,scale:0,borderRadius:"0% 0% 0 100%" }) : (backdrop = { opacity: 1,borderRadius:"0",scale:1 });
   return (
     <>
-      <Card initial={{opacity:0,scale:0,y:"-50%",borderRadius:"100%"}} 
-      animate={{opacity:1,scale:1,borderRadius:"1rem",y:"50%"}} 
-      className={`z-50 fixed top-1/2 left-0 right-0 transform -translate-y-1/2  mx-8  max-w-lg transition-all ease-linear delay-500 duration-300 bg-white dark:bg-black dark:border dark:border-white/40 dark:backdrop-blur dark:text-white ${className}`}>
-       {children}
-      </Card>
-      <motion.div 
-      initial={{opacity:0}}
-      animate={backdrop}
-        className="fixed top-0 bottom-0 left-0 backdrop-blur dark:backdrop-blur-none right-0 bg-black/30  z-40 cursor-pointer"
+      <motion.div
+        initial={{ opacity: 0, scale: 0, y: "0%", borderRadius: "100%" }}
+        animate={{ opacity: 1, scale: isShown?1:0, borderRadius: "1rem", y:  isShown?"-50%":"-100%"}}
+        transition={{delay:isShown?0.5:0.3,duration:.4}}
+        className={`z-50 fixed top-1/2 left-0 right-0 mx-8 p-4  max-w-lg bg-white/80 backdrop-blur-xl  backdrop-saturate-50  dark:bg-black/80   dark:backdrop-blur dark:text-white ${className}`}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 ,borderRadius:"10rem",scale:0}}
+        transition={{delay:isShown?0.5:0.3,duration:.4}}
+        animate={backdrop}
+        className="fixed top-0 bottom-0 left-0 backdrop-blur  right-0 bg-black/30 z-40 origin-top-right cursor-pointer"
         onClick={onExit}
       />
     </>
