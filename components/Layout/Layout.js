07f643a008cac20d21cdcf11ee3a77731eb30157
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-25 13:20:46 +0430

    [BUGFIXED] layout footer

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,20 +1,27 @@
 import Navbar from "./NavBar/NavBar";
 import Footer from "./Footer/Footer";
 import { motion } from "framer-motion";
-const Layout = ({ children, footer=true, header=true ,headerTransparent}) => {
+const Layout = ({
+  children,
+  footer = true,
+  header = true,
+  headerTransparent,
+}) => {
   return (
     <div className={`relative dark:bg-[#121212] `}>
-      {header && <Navbar headerTransparent={headerTransparent}/>}
-      <motion.main initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.75,when:"beforeChildren",staggerChildren:5}} className={`
-     ${header?"pt-20":""}
-     ${footer?"pb-96":""}
-       px-2
-       sm:pt-28
-       sm:pb-60
-         bg-[#fefefe] dark:bg-[#121212]`}>
-        <div className="min-h-screen relative">
-        {children}
-        </div>
+      {header && <Navbar headerTransparent={headerTransparent} />}
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 0.75,
+          when: "beforeChildren",
+          staggerChildren: 5,
+        }}
+        className={`
+     ${header ? "pt-4" : ""} pb-8 px-2 sm:pt-4 bg-[#fefefe] dark:bg-[#121212]`}
+      >
+        <div className="min-h-screen relative">{children}</div>
       </motion.main>
       {footer && <Footer />}
     </div>
